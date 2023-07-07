451b6976af6279767563ac0f98ac135fa896df01
AndrewNarlock <95453161+AndrewNarlock@users.noreply.github.com>
2023-07-07 16:59:29 -0700

    Update animationScript.js
    Added touchscreen movement

--- scripts/animationScript.js
+++ scripts/animationScript.js
@@ -55,6 +55,34 @@ ufo.addEventListener('mouseup', () => {
     isDragging = false;
 });
 
+ufo.addEventListener('touchstart', (e) => {
+    isDragging = true;
+    offsetTouchX = ufo.offsetLeft - e.touches[0].clientX;
+    offsetTouchY = ufo.offsetTop - e.touches[0].clientY;
+});
+
+ufo.addEventListener('touchend', () => {
+    if (ufoDisk.getBoundingClientRect().bottom >= groundContainer.getBoundingClientRect().top) {
+        ufo.style.top = `${1090}px`;
+    }
+    isDragging = false;
+});
+
+ufo.addEventListener('touchmove', (e) => {
+    if (isDragging) {
+        if (ufoDisk.getBoundingClientRect().bottom >= groundContainer.getBoundingClientRect().top) {
+            ufo.style.left = `${e.touches[0].clientX + offsetTouchX}px`;
+            ground.style.left = `${e.clientY - (ground.getBoundingClientRect().width / 2) + 10}px`;
+        } else {
+            ufo.style.left = `${e.touches[0].clientX + offsetTouchX}px`;
+            ufo.style.top = `${e.touches[0].clientY + offsetTouchY}px`;
+            ground.style.left = `${e.clientY - (ground.getBoundingClientRect().width / 2) + 10}px`;
+            ufoLight.style.width = `${e.touches[0].clientY + 300}px`;
+            ground.style.width = `${e.touches[0].clientY + 700}px`;
+        }
+    };
+});
+
 document.addEventListener('mouseup', () => {
     if (ufoDisk.getBoundingClientRect().bottom >= groundContainer.getBoundingClientRect().top) {
         ufo.style.top = `${1090}px`;
@@ -74,7 +102,6 @@ document.addEventListener('mousemove', (e) => {
             ufoLight.style.width = `${e.clientY + 300}px`;
             ground.style.width = `${e.clientY + 700}px`;
         }
-
     };
 });
 
@@ -82,6 +109,7 @@ window.addEventListener('load', () => {
     ufo.style.left = `${windowXPos - (ufoSpec.width / 2)}px`;
     ufo.style.top = `${(window.innerHeight / 2) + 200}px`;
     ufoLight.style.width = `${200}px`;
+    ufo.style.touchAction = 'none';
     ground.style.width = `${800}px`;
     moveLetters();
     placeStars();
